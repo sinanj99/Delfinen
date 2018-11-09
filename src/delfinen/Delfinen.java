81fbbcbcce57fff3070ca3937839bca3959aa84b
sinanjasar <sinanjasar@192.168.0.10>
2018-11-09 17:51:18 +0100

    new commit

---
+++ src/delfinen/Delfinen.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package delfinen;
+
+/**
+ *
+ * @author sinanjasar
+ */
+public class Delfinen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("de der racister de kommer fra alle sider");
+    }
+    
+}
