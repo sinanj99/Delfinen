4f07c0e70503c4bb799936542394e88be6215c95
sinanjasar <sinanjasar@192.168.0.10>
2018-11-09 18:00:56 +0100

    new commit

--- src/delfinen/Delfinen.java
+++ src/delfinen/Delfinen.java
@@ -15,7 +15,7 @@ public class Delfinen {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("din far");
+        System.out.println("sne");
     }
     
 }
